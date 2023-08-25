77d5e114a68815f0e346f737c6ae8ae4d4a57080
makmodol1173 <makmodol1173@gmail.com>
2023-08-25 22:32:24 +0600

    Multiple Cleint Send Message to Server

--- Server.java
+++ Server.java
@@ -1,55 +1,66 @@
 import java.net.*;
 import java.io.*;
 
-class Server
-{
-	 Socket socket = null;
-	 ServerSocket server = null;
-	 DataInputStream in = null;
+class Server {
+    ServerSocket server = null;
 
-	Server(int port)
-	{
-		try
-		{
-			server = new ServerSocket(port);
-			System.out.println("Server started");
+    Server(int port) {
+        try {
+            server = new ServerSocket(port);
+            System.out.println("Server started");
 
-			System.out.println("Waiting for a client ...");
+            while (true) {
+                System.out.println("Waiting for a client ...");
+                Socket socket = server.accept();
+                System.out.println("Client accepted");
 
-			socket = server.accept();
-			System.out.println("Client accepted");
+                // Create a new thread to handle the client
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 
-			in = new DataInputStream(
-				new BufferedInputStream(socket.getInputStream()));
+    public static void main(String args[]) {
+        Server server = new Server(5000);
+    }
 
-			String line = "";
+    // Inner class to handle a client in a separate thread
+    private static class ClientHandler implements Runnable {
+        private Socket socket;
+        private DataInputStream in;
 
-			while (!line.equals("Over"))
-			{
-				try
-				{
-					line = in.readUTF();
-					System.out.println(line);
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
 
-				}
-				catch(IOException e)
-				{
-					System.out.println(e);
-				}
-			}
-			System.out.println("Closing connection");
+        @Override
+        public void run() {
+            try {
+                in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
 
-			socket.close();
-			in.close();
-		}
-		catch(IOException e)
-		{
-			System.out.println(e);
-		}
-	}
+                String line = "";
+                while (!line.equals("Over")) {
+                    try {
+                        line = in.readUTF();
+                        System.out.println(line);
 
-	public static void main(String args[])
-	{
-		Server server = new Server(5000);
-	}
+                        // You can add code here to send data back to the client if needed
+
+                    } catch (IOException e) {
+                        System.out.println(e);
+                    }
+                }
+                System.out.println("Closing connection");
+
+                socket.close();
+                in.close();
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+    }
 }
